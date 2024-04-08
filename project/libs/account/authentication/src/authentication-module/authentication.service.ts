5d51dc602b3264ec38af341e149dd9d8598d35b8
Elraden <morozovsa94@yandex.ru>
2024-04-08 20:36:41 +0300

    Добавлен метод verifyUser в AuthenticationService.

--- project/libs/account/authentication/src/authentication-module/authentication.service.ts
+++ project/libs/account/authentication/src/authentication-module/authentication.service.ts
@@ -1,7 +1,8 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { BlogUserRepository, BlogUserEntity } from '@project/blog-user';
 import { CreateUserDto } from '../dto/create-user.dto';
-import { AUTH_USER_EXISTS } from './authentication.constant';
+import { AUTH_USER_EXISTS, AUTH_USER_NOT_FOUND, AUTH_USER_PASSWORD_WRONG } from './authentication.constant';
+import { LoginUserDto } from '../dto/login-user.dto';
 
 @Injectable()
 export class AuthenticationService {
@@ -28,5 +29,30 @@ export class AuthenticationService {
         const userEntity = await new BlogUserEntity(blogUser).setPassword(password);
 
         return this.blogUserRepository.save(userEntity);
+    }
+
+    public async verifyUser(dto: LoginUserDto) {
+        const {email, password} = dto;
+        const existUser = await this.blogUserRepository.findByEmail(email);
+        
+        if(!existUser){
+            throw new NotFoundException(AUTH_USER_NOT_FOUND);
+        }
+
+        if(!await existUser.comparePassword(password)) {
+            throw new UnauthorizedException(AUTH_USER_PASSWORD_WRONG);
+        }
+
+        return existUser;
+    }
+
+    public async getUser(id: string) {
+        const user = await this.blogUserRepository.findById(id);
+
+        if (!user) {
+            throw new NotFoundException(AUTH_USER_NOT_FOUND);
+        }
+
+        return user;
     }
 }
