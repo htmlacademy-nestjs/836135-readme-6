f85e23afd841ec489826e559be42bc0caec0ff45
Elraden <morozovsa94@yandex.ru>
2024-04-08 20:09:42 +0300

    Добавлен метод comparePassword

--- project/libs/account/blog-user/src/blog-user-module/blog-user.entity.ts
+++ project/libs/account/blog-user/src/blog-user-module/blog-user.entity.ts
@@ -1,4 +1,4 @@
-import { genSalt, hash } from 'bcrypt';
+import { compare, genSalt, hash } from 'bcrypt';
 import { Entity } from '@project/shared/core';
 import { StorableEntity, AuthUser } from '@project/shared/core';
 import { SALT_ROUNDS } from './blog-user.constant';
@@ -37,5 +37,9 @@ export class BlogUserEntity extends Entity implements StorableEntity<AuthUser> {
         const salt = await genSalt(SALT_ROUNDS);
         this.passwordHash = await hash(password, salt);
         return this;
+    }
+
+    public async comparePassword(password: string): Promise<boolean> {
+        return compare(password, this.passwordHash);
     }
 }
