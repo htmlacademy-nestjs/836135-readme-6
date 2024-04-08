658a37474106636578f25b07952bcc8b016551a6
Elraden <morozovsa94@yandex.ru>
2024-04-08 19:56:00 +0300

    Репозиторий внедрен в качестве зависимости.

--- project/libs/account/authentication/src/authentication-module/authentication-module.ts
+++ project/libs/account/authentication/src/authentication-module/authentication-module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { BlogUserModule } from '@project/blog-user';
 import { AuthenticationController } from './authentication.controller';
 import { AuthenticationService } from './authentication.service';
 
 @Module({
+    imports: [BlogUserModule],
     controllers: [AuthenticationController],
     providers: [AuthenticationService]
 })
