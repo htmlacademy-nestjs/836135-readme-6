7d856cef14de738a002de8c52d9d98dda637d429
Elraden <morozovsa94@yandex.ru>
2024-04-16 21:27:26 +0300

    Исправлены ошибки

--- project/libs/account/config/src/configurations/mongodb/get-mongoose-options.ts
+++ project/libs/account/config/src/configurations/mongodb/get-mongoose-options.ts
@@ -11,7 +11,7 @@ export function getMongooseOptions(): MongooseModuleAsyncOptions {
                     password: config.get<string>('db.password'),
                     host: config.get<string>('db.host'),
                     port: config.get<string>('db.port'),
-                    authDatabase: config.get<string>('db.authDatabase'),
+                    authDatabase: config.get<string>('db.authBase'),
                     databaseName: config.get<string>('db.databaseName'),
                 })
             }
