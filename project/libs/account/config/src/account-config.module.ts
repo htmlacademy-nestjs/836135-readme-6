eea33ebed45d8ad141baa35fe3adb893b3520deb
Elraden <morozovsa94@yandex.ru>
2024-04-16 19:12:19 +0300

    Добавлено пространство db для настроен

--- project/libs/account/config/src/account-config.module.ts
+++ project/libs/account/config/src/account-config.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
 import applicationConfig from './configurations/app.config';
+import mongoConfig from './configurations/mongo.config';
 
 const ENV_USERS_FILE_PATH = 'apps/account/account.env';
 
@@ -11,7 +12,7 @@ const ENV_USERS_FILE_PATH = 'apps/account/account.env';
             isGlobal: true,
             cache: true,
             // TODO: Передать список конфигураций для загрузки
-            load: [applicationConfig],
+            load: [applicationConfig, mongoConfig],
             envFilePath: ENV_USERS_FILE_PATH
         }),
     ]
